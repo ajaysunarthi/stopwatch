2d9f18b50e98e56527ab4a5eb9def0663b707055
ajay <ajaysunarthi@iitj.ac.in>
2016-05-30 09:16:01 +0530

    start timer added

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,64 @@
 /**
-*  Module
-*
-* Description
-*/
-(function(){
+ *  Module
+ *
+ * Description
+ */
+(function() {
 
-angular.module('stopWatch', []).
-controller('appCtrl', appController);
+    angular.module('stopWatch', []).
+    controller('appCtrl', appController);
 
-function appController ($scope) {
-	var vm = this;
+    function appController($scope) {
+        var vm = this;
 
-	vm.title = "stopwatch";
-}
+        init();
 
-appController.$inject = ['$scope'];
-	
+        function init() {
+            vm.timer = {
+                hours: 0,
+                minutes: 0,
+                seconds: 0,
+                ms: 0
+            };
+        }
+
+        vm.laps = [];
+        vm.splits = [];
+
+        var interval, startTime = 0,
+            timeElapsed, pause = -999,
+            splitindex = 0,
+            lapindex = 0;
+        var time = {};
+
+        vm.startTimer = function() {
+
+            if (!(vm.timer.ms || vm.timer.seconds || vm.timer.ms)) {
+                startTime = Date.now();
+            } else if (pause) {
+                startTime = Date.now() + startTime - pause;
+            } else {
+                return;
+            }
+            pause = 0;
+
+            interval = setInterval(function() {
+                $scope.$apply(function() {
+                    timeElapsed = Date.now() - startTime;
+
+                    vm.timer = {
+                        ms: timeElapsed % 1000,
+                        seconds: (Math.floor(timeElapsed / 1000)) % 60,
+                        minutes: 0 ? (this.seconds < 59) : (Math.floor((timeElapsed / 1000) / 60)) % 60,
+                        hours: 0 ? (this.minutes < 59) : (Math.floor(((timeElapsed / 1000) / 60) / 60)) % 60
+                    };
+                });
+            }, 1);
+        };
+
+
+    }
+
+    appController.$inject = ['$scope'];
+
 })();
